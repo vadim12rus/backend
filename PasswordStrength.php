5c56328bf4a0680a77809de6c827ca7b91ed7403
vadim12rus <vadim.dolganof@gmail.com>
2016-05-31 15:57:48 +0300

    Update PasswordStrength.php

--- PasswordStrength.php
+++ PasswordStrength.php
@@ -2,5 +2,13 @@
     require_once('include/string.inc.php');
     if (checkStr($_GET['password']))
     {
-        echo "Надежность пароля ".PasswordStrength($_GET['password']);
+        if (checkStr($_GET['password']))
+        {
+            echo "Надежность пароля ".PasswordStrength($_GET['password']);
+        }
     }
+    else
+    {
+        echo "Некорректный ввод!!!";
+    }
+    
